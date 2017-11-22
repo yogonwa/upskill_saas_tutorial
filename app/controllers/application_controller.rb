66ba1671c5be6783d87a39b9706086093200911f
Joe Gonwa <joegonwa@gmail.com>
2017-11-22 23:27:58 +0000

    Updated flash message styles and js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
+  # If coming from a devise sign-up form, 
+  # Whitelist the following form fields so that we can process them
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
     def configure_permitted_parameters
