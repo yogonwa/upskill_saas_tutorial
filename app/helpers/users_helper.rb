f1afd7361402dd13f3f9b2178991993ab7900054
Joe Gonwa <joegonwa@gmail.com>
2017-12-02 22:36:18 +0000

    BugFix: damn closing bracket

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,9 +3,9 @@ module UsersHelper
     if @user.profile.job_title == "Developer"
       "<i class='fa fa-code'></i>".html_safe
     elsif @user.profile.job_title == "Entrepreneur"
-      "<i class='fa fa-lightbulb-o'><i/>".html_safe
+      "<i class='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
-      "<i class='fa fa-dollaro'><i/>".html_safe
+      "<i class='fa fa-dollar'></i>".html_safe
     end
   end
 end
