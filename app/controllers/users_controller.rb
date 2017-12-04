c7573104719acf6bce1d3ba6b7691c0a9e018afd
Joe Gonwa <joegonwa@gmail.com>
2017-12-04 17:47:36 +0000

    Added user  index controller and barebones index view page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
+  # When GET is made to /users/
+  def index
+    
+  end
+  
   
   # When GET is made to /users/:id
   def show
     @user = User.find( params[:id] )
   end
+  
+  
+  
   
 end
